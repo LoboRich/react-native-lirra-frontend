a037d7215ef662735e0d2d5ce60348a1c5f2443d
Rich Lobo <lobo.r0018@gmail.com>
2025-09-04 18:04:54 +0800

    Adds constants colors and test to indedx

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,26 @@
-import { Text, View } from "react-native";
-
+import { StyleSheet, Text, View } from "react-native";
+import { Image } from "expo-image";
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={styles.title}>Hello</Text>
+      <Image source={require("../assets/images/research.svg")} style={{ width: 100, height: 100 }} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "red",
+  }
+});
