28e39aebc843871e3f908d62de66283cd678659b
Rich Lobo <lobo.r0018@gmail.com>
2025-09-05 17:19:22 +0800

    Adds Safescreen component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,15 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import SafeScreen from "../components/SafeScreen";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <SafeScreen>
+        <Stack screenOptions={{ headerShown: false }}/>
+      </SafeScreen>
+      <StatusBar style="dark" />
+    </SafeAreaProvider>
+  );
 }
