6a6db91e55ce10eef7bc553119a61d6dd1632439
Rich Lobo <lobo.r0018@gmail.com>
2025-09-11 14:14:34 +0800

    Uses API_URL constants

---
+++ constants/api.js
@@ -0,0 +1,1 @@
+export const API_URL = "https://react-native-lirra-backend.onrender.com/api";
--- store/authStore.js
+++ store/authStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { API_URL } from "../constants/api";
 
 export const useAuthStore = create((set) => ({
     user: null,
@@ -10,7 +11,7 @@ export const useAuthStore = create((set) => ({
         set({isLoading: true});
 
         try{
-            const response = await fetch('https://react-native-lirra-backend.onrender.com/api/auth/register', {
+            const response = await fetch(`${API_URL}/auth/register`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -41,7 +42,7 @@ export const useAuthStore = create((set) => ({
         set({isLoading: true});
 
         try{
-            const response = await fetch('https://react-native-lirra-backend.onrender.com/api/auth/login', {
+            const response = await fetch(`${API_URL}/auth/login`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
